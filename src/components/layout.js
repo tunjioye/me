d5d9736757492bf5d760ef9f2db7121ae6a348d7
Tunji Oyeniran <oyenirantunji2339@gmail.com>
2019-08-14 08:31:53 +0100

    Home Page Implementation

--- src/components/layout.js
+++ src/components/layout.js
@@ -34,7 +34,7 @@ const Layout = ({ children }) => {
           paddingTop: 0,
         }}
       >
-        <main>{children}</main>
+        <main className="main-content">{children}</main>
         <footer>
           © {new Date().getFullYear()}, Built with
           {` `}
