9c9e02bfe782a77b26ed0f7ef420b62ec8bae1f7
Tunji Oyeniran <oyenirantunji2339@gmail.com>
2019-08-14 08:43:21 +0100

    moved siteMetadata into me-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
+const { siteMetadata } = require('./me-config')
+
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    ...siteMetadata
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
--- me-config.js
+++ me-config.js
@@ -14,4 +14,9 @@ module.exports = {
       title: `Resume`,
     },
   ],
+  siteMetadata: {
+    title: `Tunji Oyeniran`,
+    description: `Tunji Oyeniran is a Fullstack Developer who loves building useful mobile friendly applications using Laravel, PHP, React, Vue and Node JS.`,
+    author: `@tunjioye`,
+  },
 }
