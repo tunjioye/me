6b0deb5a41ed94e5f14fc9627a6af050a29fac47
Tunji Oyeniran <oyenirantunji2339@gmail.com>
2020-02-11 12:55:12 +0100

    UPDATE : added footer component

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,7 +10,7 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
-import SocialLink from "./social-link"
+import Footer from "./footer"
 import "./layout.css"
 import { contacts } from "./../../me-config"
 
@@ -28,42 +28,23 @@ const Layout = ({ children }) => {
   return (
     <>
       <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
+      <div style={containerStyle}>
         <br />
-
         <main className="main-content">{children}</main>
-
         <br />
         <br />
 
-        <footer>
-          <div>
-            <SocialLink href={`mailto:${contacts.email || ''}`}>Email</SocialLink>
-            <SocialLink href={`${contacts.github || ''}`}>Github</SocialLink>
-            <SocialLink href={`${contacts.twitter || ''}`}>Twitter</SocialLink>
-            <SocialLink href={`${contacts.linkedin || ''}`}>LinkedIn</SocialLink>
-            <SocialLink href={`${contacts.instagram || ''}`}>Instagram</SocialLink>
-            <SocialLink href={`${contacts.stackoverflow || ''}`}>Stackoverflow</SocialLink>
-            <SocialLink href={`${contacts.medium || ''}`}>Medium</SocialLink>
-          </div>
-          <br />
-          <div>
-            <small>
-              <strong>&copy;</strong> {new Date().getFullYear()},
-              Built with <a href="https://www.gatsbyjs.org" target="_blank" rel="noopener noreferrer">Gatsby</a>
-            </small>
-          </div>
-        </footer>
+        <Footer contacts={contacts} />
       </div>
     </>
   )
+}
+
+const containerStyle = {
+  margin: `0 auto`,
+  maxWidth: 960,
+  padding: `0px 1.0875rem 1.45rem`,
+  paddingTop: 0,
 }
 
 Layout.propTypes = {
